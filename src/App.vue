1848207bda7338a14dd8d85fb9bcbe1654743c34
Daniel Hogan <daniel.hogan@usask.ca>
2017-05-14 23:52:59 -0600

    Add simple changes. Still learning.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
+    <div id="header">
+      <h1>{{ intro }}</h1>
+    </div>
+    <p>I'm just a guy who likes to build things, especially with code. To find out more, please check out some of these linked.</p>
+    <h2>Find out more..</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    {{ msg }}
   </div>
 </template>
 
@@ -25,7 +19,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      intro: 'Hi, I\'m Daniel'
     }
   }
 }
