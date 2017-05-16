839cb4accf38b2ee7b49f1308a98b49a7885b787
Daniel Hogan <daniel.hogan@usask.ca>
2017-05-15 21:32:24 -0600

    Install routing packag and change the colours a bit.

---
+++ src/Skills.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="skills">
+    <div v-for="skill in skills">
+      {{ skill.category }}
+      <div v-for="tech in skill.techs">
+        {{ tech }} 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'skills',
+  data: {
+    skills: [
+      {
+        category: "Big-Data",
+        techs: [
+          "Weka",
+          "TensorFlow",
+        ],
+      },
+      {
+        category: "Mobile",
+        techs: [
+          "Android Studio",
+        ],
+      },
+      {
+        category: "Systems",
+        techs: [
+          "C",
+          "C++",
+          "Java",
+        ],
+      },
+      { 
+        category: "Scripting",
+        techs: [
+          "Python",
+          "Ruby",
+          "Perl",
+        ],
+      },
+    ],
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Home = { template: '<div>home</div>' }
+const Foo = { template: '<div>foo</div>' }
+
+
 new Vue({
   el: '#app',
+  data: jk
   render: h => h(App)
 })
